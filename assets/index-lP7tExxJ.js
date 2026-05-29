c8caef87b9eb681d0242fd43619b1865b9e585fe
DowCloth Deploy <deploy@dowcloth.app>
2026-05-29 09:32:22 +0000

    Fix images + smart canvas try-on with body zone detection

--- assets/index-lP7tExxJ.js
+++ assets/index-lP7tExxJ.js
@@ -1,4 +1,4 @@
-import{a as CS,c as so,g as kS}from"./index-DUuu5TOQ.js";function TS(n,e){for(var t=0;t<e.length;t++){const s=e[t];if(typeof s!="string"&&!Array.isArray(s)){for(const r in s)if(r!=="default"&&!(r in n)){const o=Object.getOwnPropertyDescriptor(s,r);o&&Object.defineProperty(n,r,o.get?o:{enumerable:!0,get:()=>s[r]})}}}return Object.freeze(Object.defineProperty(n,Symbol.toStringTag,{value:"Module"}))}/**
+import{a as CS,c as so,g as kS}from"./index-DaZFrHcF.js";function TS(n,e){for(var t=0;t<e.length;t++){const s=e[t];if(typeof s!="string"&&!Array.isArray(s)){for(const r in s)if(r!=="default"&&!(r in n)){const o=Object.getOwnPropertyDescriptor(s,r);o&&Object.defineProperty(n,r,o.get?o:{enumerable:!0,get:()=>s[r]})}}}return Object.freeze(Object.defineProperty(n,Symbol.toStringTag,{value:"Module"}))}/**
  * @license
  * Copyright 2020 Google LLC. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
